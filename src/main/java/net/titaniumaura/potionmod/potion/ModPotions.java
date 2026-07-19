411364f4823d71cbc4a82179a87a20564728ea8e
Titanium-Aura <jacomagata@gmail.com>
2026-07-19 15:56:07 -0300

    Made lightining potion <3

--- src/main/java/net/titaniumaura/potionmod/potion/ModPotions.java
+++ src/main/java/net/titaniumaura/potionmod/potion/ModPotions.java
@@ -1,17 +1,20 @@
 package net.titaniumaura.potionmod.potion;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.alchemy.Potion;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.titaniumaura.potionmod.PotionMod;
+import net.titaniumaura.potionmod.effect.ModEffects;
 
 public class ModPotions {
     public static final DeferredRegister<Potion> POTIONS =
             DeferredRegister.create(BuiltInRegistries.POTION, PotionMod.MODID);
 
-
-
+    public static final Holder<Potion> LIGHTNING = POTIONS.register("lightning_potion",
+            () -> new Potion(new MobEffectInstance(ModEffects.LIGHTNING)));
 
 
     public static void register(IEventBus eventBus) {
