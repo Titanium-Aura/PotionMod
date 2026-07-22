9423497ce8e365b40eb42d6a73345fef488f2e19
NekoPancakes <Midnightmoon3015@gmail.com>
2026-07-22 10:07:21 -0700

    lightning potion is finished

--- src/main/java/net/titaniumaura/potionmod/effect/LightningEffect.java
+++ src/main/java/net/titaniumaura/potionmod/effect/LightningEffect.java
@@ -14,6 +14,7 @@ import net.minecraft.world.phys.AABB;
 import javax.annotation.Nullable;
 import java.util.List;
 
+
 public class LightningEffect extends InstantenousSplashAndLingeringEffect {
     public LightningEffect(MobEffectCategory category, int color) {
         super(category, color);
