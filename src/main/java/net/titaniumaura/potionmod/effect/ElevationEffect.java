37cd1e2144a7526d0c2283cc989ae16e08533e4e
NekoPancakes <Midnightmoon3015@gmail.com>
2026-07-23 11:01:02 -0700

    Elevation potion should be working

--- src/main/java/net/titaniumaura/potionmod/effect/ElevationEffect.java
+++ src/main/java/net/titaniumaura/potionmod/effect/ElevationEffect.java
@@ -21,30 +21,39 @@ public class ElevationEffect extends InstantenousMobEffect {
     @Override
     public boolean applyEffectTick(LivingEntity livingEntity, int amplifier) {
         Vec3 pos = livingEntity.position();
-        livingEntity.teleportTo(pos.x, pos.y + 50 * (amplifier + 1), pos.z);
+        livingEntity.teleportTo(pos.x, pos.y, pos.z);
         return true;
     }
 
 
     @Override
     public void applyInstantenousEffect(@Nullable Entity source, @Nullable Entity indirectSource, LivingEntity livingEntity, int amplifier, double health) {
-        Vec3 pos = livingEntity.position();
-        livingEntity.teleportTo(pos.x, pos.y + 50 * (amplifier + 1), pos.z);
+        Vec3 pos = getNearestValidPosition(livingEntity.level(), livingEntity.position(), 50 * (amplifier + 1));
+        livingEntity.teleportTo(pos.x, pos.y, pos.z);
 
     }
 
 
-    private Vec3 getNearestValidPostion(Level level, Vec3 pos, int teleportAmount) {
+    private Vec3 getNearestValidPosition(Level level, Vec3 pos, int teleportAmount) {
         int airBlocks = 0;
+        boolean fullBlockCheck = false;
         for (int i = 0; i < teleportAmount; i++) {
-            if (!level.getBlockState(new BlockPos((int) pos.x, (int) pos.y + i, (int) pos.z)).is(Blocks.AIR)) {
-                airBlocks = 0;
+            if (fullBlockCheck == true) {
+                if (level.getBlockState(new BlockPos((int) pos.x, (int) pos.y + i, (int) pos.z)).is(Blocks.AIR)) {
+                    airBlocks += 1;
+                } else {
+                    airBlocks = 0;
+                }
+                if (airBlocks == 2) {
+                    return new Vec3(pos.x, pos.y + i, pos.z);
+                }
+
             } else {
-                airBlocks += 1;
+                if (!level.getBlockState(new BlockPos((int) pos.x, (int) pos.y + i, (int) pos.z)).is(Blocks.AIR)) {
+                    fullBlockCheck = true;
+                }
             }
-            if (airBlocks == 2) {
-                return new Vec3(pos.x, pos.y + i, pos.z);
-            }
+
         }
         return new Vec3(pos.x, pos.y + teleportAmount, pos.z);
     }
